7637530e7b391d1e079d1c01eb84d5d4b365e8d3
RMatos <ricardo.matos29@gmail.com>
2016-05-12 21:20:48 +0100

    added missing model fields

--- Utils/variables.py
+++ Utils/variables.py
@@ -9,6 +9,7 @@ class Variables(object):
     CONFIG_DIR = "./yaml"
     ZOOM_FACTOR = 1
     NUMBER_OF_CLASSES = -1
+    MODEL_IMAGE_SIZE = 180  #@TODO -1 by default
     classes = "empty"
     layers_checkboxes = []
 
